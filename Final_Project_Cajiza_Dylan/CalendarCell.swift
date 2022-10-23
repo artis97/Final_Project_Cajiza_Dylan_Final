4b2312a0fe4af0ab1793c52c888c8d8d6e0f7a25
Aristoteles Cajiza <cajiza2000@hotmail.com>
2022-10-23 14:55:27 -0700

    Weekly2

---
+++ Final_Project_Cajiza_Dylan/CalendarCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCell.swift
+//  Final_Project_Cajiza_Dylan
+//
+//  Created by Aristoteles on 10/23/23.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+	@IBOutlet weak var dayOfMonth: UILabel!
+}
